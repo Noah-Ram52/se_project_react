749cf3f97983edce0adddab5f64310d8947743f0
Noah Ramos <merawe481@gmail.com>
2025-05-27 23:30:30 -0500

    Styling Weather Text Cards

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,8 +1,10 @@
+import "./ItemCard.css";
+
 function ItemCard({ item }) {
   return (
     <div>
-      <h2>{item.name}</h2>
-      <img src={item.link} alt={item.name} />
+      <h2 className="weather__cards-text">{item.name}</h2>
+      <img src={item.link} alt={item.name} className="weather__images" />
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,7 @@ function Main({ weatherData }) {
     <main>
       <WeatherCard />
       <section className="cards">
-        <p className="cards__text">
+        <p className="weather__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
         <ul className="cards__list">
