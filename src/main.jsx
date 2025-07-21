5f4c406d4acf63b2ed3580e1296d574ae3c8c4e0
Noah Ramos <merawe481@gmail.com>
2025-07-20 23:39:27 -0500

    Final Project 11.3

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App.jsx";
 import "./index.css";
 
+// Get basename from environment - empty for development, /se_project_react for production
+const basename = import.meta.env.PROD ? "/se_project_react" : "";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
